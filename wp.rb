05f0ed0203fcaec6da3f3429e33ece12b3f78551
Amar1729 <amar.paul16@gmail.com>
2020-03-09 20:01:15 -0400

    wp: init (HEAD)

---
+++ wp.rb
@@ -0,0 +1,18 @@
+class Wp < Formula
+  desc "Wrapper script for pywal with per-desktop support and other bells and whistles"
+  homepage ""
+  sha256 "ff61efca6bb11ff4fc641eabd6532678169c4da4eedbd3686a45815952baf303"
+  head "https://github.com/Amar1729/wp.git"
+
+  bottle :unneeded
+
+  depends_on "coreutils"
+
+  def install
+    bin.install "wp"
+  end
+
+  test do
+    system "wp"
+  end
+end
