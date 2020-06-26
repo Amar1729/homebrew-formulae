ce19e14e61899ad5eb8b04669f35f4810af4413b
Amar1729 <amar.paul16@gmail.com>
2020-06-26 02:13:21 -0400

    pinentry-prefer-tty : init as gist

---
+++ pinentry-prefer-tty.rb
@@ -0,0 +1,19 @@
+class PinentryPreferTty < Formula
+  desc "Prefer the tty when using pinentry, fallback to GUI"
+  homepage ""
+  sha256 "6c6d4794a1163b231e7c08e4ef20d470cd33717c6cc29bf88ff0a2d1d7ef7468"
+  version 'ac127e57451a67e6ed9740857fbbd6f742534a99'
+  url "https://gist.githubusercontent.com/Amar1729/18cbcf7e78b84e4097ee6b1c56b65b6f/raw/#{version}/pinentry-prefer-tty"
+
+  depends_on "pinentry"
+  depends_on "pinentry-mac"
+
+  bottle :unneeded
+
+  def install
+    bin.install "pinentry-prefer-tty"
+    inreplace bin/"pinentry-prefer-tty", "/usr/bin/pinentry-curses", "#{Formula["pinentry"].opt_bin}/pinentry-curses"
+    inreplace bin/"pinentry-prefer-tty", "/usr/bin/pinentry-mac", "#{Formula["pinentry-mac"].opt_bin}/pinentry-mac"
+  end
+end
+
