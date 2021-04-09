ae03f69643badb2181e1cca8fecc9261170308a5
Amar1729 <amar.paul16@gmail.com>
2021-04-09 13:24:21 -0400

    browserpass : run brew style --fix since homebrew warns on brew update

--- browserpass.rb
+++ browserpass.rb
@@ -1,19 +1,13 @@
 class Browserpass < Formula
-  version "3.0.7"
-  desc "This is a host application for browserpass browser extension providing it access to your password store."
+  desc "Host application for browser extension providing access to your password store"
   homepage "https://github.com/browserpass/browserpass-native"
   url "https://github.com/browserpass/browserpass-native/releases/download/#{version}/browserpass-darwin64-#{version}.tar.gz"
+  version "3.0.7"
   sha256 "97b9a9068a3c88fb1d52d42a1712e199da5865a4c6f8352b9fe3eae1ee86c746"
 
   bottle do
     root_url "https://github.com/Amar1729/homebrew-formulae/releases/download/browserpass-3.0.7"
-    cellar :any_skip_relocation
-    sha256 "0d1b3c9497047ee387e154df75da8d90af9327b80af35391aeb02811c01d6e7a" => :catalina
-  end
-
-  resource "testfile" do
-    url "https://github.com/browserpass/browserpass-native/files/3062744/request.hex.txt"
-    sha256 "83ea960015e5bd05e604c13233d8ed16a87c38f83212822e8d69c622dea21af0"
+    sha256 cellar: :any_skip_relocation, catalina: "0d1b3c9497047ee387e154df75da8d90af9327b80af35391aeb02811c01d6e7a"
   end
 
   depends_on "coreutils" => :build
@@ -21,6 +15,11 @@ class Browserpass < Formula
   depends_on "gpg"
   depends_on "pinentry"
   depends_on "pinentry-mac"
+
+  resource "testfile" do
+    url "https://github.com/browserpass/browserpass-native/files/3062744/request.hex.txt"
+    sha256 "83ea960015e5bd05e604c13233d8ed16a87c38f83212822e8d69c622dea21af0"
+  end
 
   def install
     ENV["DESTDIR"] = ""
@@ -37,13 +36,13 @@ class Browserpass < Formula
 
   def caveats
     <<~EOF
-  ********************************************************************************
-  * To configure your browser, RUN THE FOLLOWING:
+      ********************************************************************************
+      * To configure your browser, RUN THE FOLLOWING:
 
-  $ PREFIX='/usr/local/opt/browserpass' make hosts-BROWSER-user -f /usr/local/opt/browserpass/lib/browserpass/Makefile
+      $ PREFIX='/usr/local/opt/browserpass' make hosts-BROWSER-user -f /usr/local/opt/browserpass/lib/browserpass/Makefile
 
-  * Where BROWSER is one of the following: [chromium chrome vivaldi brave firefox]
-  ********************************************************************************
+      * Where BROWSER is one of the following: [chromium chrome vivaldi brave firefox]
+      ********************************************************************************
     EOF
   end
 
