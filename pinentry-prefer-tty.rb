a9299e7caca0c7fe520a431adc579137fab51f6c
Amar1729 <amar.paul16@gmail.com>
2021-04-09 13:37:36 -0400

    pinentry-prefer-tty : run brew style --fix since homebrew warns on brew update

--- pinentry-prefer-tty.rb
+++ pinentry-prefer-tty.rb
@@ -1,14 +1,14 @@
 class PinentryPreferTty < Formula
   desc "Prefer the tty when using pinentry, fallback to GUI"
-  homepage ""
-  sha256 "6c6d4794a1163b231e7c08e4ef20d470cd33717c6cc29bf88ff0a2d1d7ef7468"
-  version 'ac127e57451a67e6ed9740857fbbd6f742534a99'
+  homepage "https://gist.github.com/Amar1729/18cbcf7e78b84e4097ee6b1c56b65b6f"
   url "https://gist.githubusercontent.com/Amar1729/18cbcf7e78b84e4097ee6b1c56b65b6f/raw/#{version}/pinentry-prefer-tty"
+  version "ac127e57451a67e6ed9740857fbbd6f742534a99"
+  sha256 "6c6d4794a1163b231e7c08e4ef20d470cd33717c6cc29bf88ff0a2d1d7ef7468"
+
+  bottle :unneeded
 
   depends_on "pinentry"
   depends_on "pinentry-mac"
-
-  bottle :unneeded
 
   def install
     bin.install "pinentry-prefer-tty"
@@ -16,4 +16,3 @@ class PinentryPreferTty < Formula
     inreplace bin/"pinentry-prefer-tty", "/usr/bin/pinentry-mac", "#{Formula["pinentry-mac"].opt_bin}/pinentry-mac"
   end
 end
-
