94cd1e1a02775fbdc419435a9677f19af6544287
Amar1729 <amar.paul16@gmail.com>
2021-10-21 01:06:51 -0400

    spec branch

--- wp.rb
+++ wp.rb
@@ -2,7 +2,7 @@ class Wp < Formula
   desc "Wrapper script for pywal with per-desktop support and other bells and whistles"
   homepage "https://github.com/Amar1729/wp"
   sha256 "ff61efca6bb11ff4fc641eabd6532678169c4da4eedbd3686a45815952baf303"
-  head "https://github.com/Amar1729/wp.git"
+  head "https://github.com/Amar1729/wp.git", branch: "master"
 
   depends_on "coreutils"
   depends_on "imagemagick"
