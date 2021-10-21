39c5816426b06c53d851004a1f3293ceb9ce073e
Amar Paul <amar.paul16@gmail.com>
2021-10-21 05:00:53 +0000

    pinentry-prefer-tty: add ac127e57451a67e6ed9740857fbbd6f742534a99 bottle.

--- pinentry-prefer-tty.rb
+++ pinentry-prefer-tty.rb
@@ -5,6 +5,11 @@ class PinentryPreferTty < Formula
   version "ac127e57451a67e6ed9740857fbbd6f742534a99"
   sha256 "6c6d4794a1163b231e7c08e4ef20d470cd33717c6cc29bf88ff0a2d1d7ef7468"
 
+  bottle do
+    root_url "https://github.com/Amar1729/homebrew-formulae/releases/download/pinentry-prefer-tty-ac127e57451a67e6ed9740857fbbd6f742534a99"
+    sha256 cellar: :any_skip_relocation, catalina: "463313f25a6dc516c28664552686c6ae95c7b54b0b31ff7461baf0513ff7e06f"
+  end
+
   depends_on "pinentry"
   depends_on "pinentry-mac"
 
