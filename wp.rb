fa3bb84d6ea20bcc8af3c595266d9b7ce39f87e0
Amar1729 <amar.paul16@gmail.com>
2021-11-02 00:24:28 -0400

    wp : bottle at v1.0
    Closes #17.
    
    Signed-off-by: Amar Paul <amar.paul16@gmail.com>

--- wp.rb
+++ wp.rb
@@ -2,9 +2,8 @@ class Wp < Formula
   desc "Wrapper script for pywal with per-desktop support and other bells and whistles"
   homepage "https://github.com/Amar1729/wp"
   sha256 "ff61efca6bb11ff4fc641eabd6532678169c4da4eedbd3686a45815952baf303"
-  head "https://github.com/Amar1729/wp.git"
-
-  bottle :unneeded
+  url "https://github.com/Amar1729/wp/archive/refs/tags/v1.0.tar.gz"
+  sha256 "04e370af6f6d00ab583ad153a17acda25d3692ee479fd847e2de475a79b36ec2"
 
   depends_on "coreutils"
   depends_on "imagemagick"
