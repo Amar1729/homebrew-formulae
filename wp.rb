d79549b9c2309afa65a806f368318bdb27148fea
Amar1729 <amar.paul16@gmail.com>
2022-01-08 02:53:24 -0500

    wp : bump to v1.1.6

--- wp.rb
+++ wp.rb
@@ -1,9 +1,8 @@
 class Wp < Formula
   desc "Wrapper script for pywal with per-desktop support and other bells and whistles"
   homepage "https://github.com/Amar1729/wp"
-  sha256 "ff61efca6bb11ff4fc641eabd6532678169c4da4eedbd3686a45815952baf303"
-  url "https://github.com/Amar1729/wp/archive/refs/tags/v1.0.tar.gz"
-  sha256 "04e370af6f6d00ab583ad153a17acda25d3692ee479fd847e2de475a79b36ec2"
+  url "https://github.com/Amar1729/wp/archive/refs/tags/v1.1.6.tar.gz"
+  sha256 "374246089fcf5ea0fc3008877f3831319e894481b1abfffe091addea9badba48"
 
   bottle do
     root_url "https://github.com/Amar1729/homebrew-formulae/releases/download/wp-1.0"
